2509bdc98f9f6574ae152d845eebaeb13aee809d
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-24 02:29:19 +0800

    Pseudocode

---
+++ odd-even-multiplier.py
@@ -0,0 +1,8 @@
+#Francis Niño A. Eugenio
+#BSCPE 1-5 | Object Oriented Programing
+#Even & Odd Multiplier
+
+# open and read the source file
+# separate the even and odd in the source file
+# create the new txt file for evem but doubled
+# create the new txt file for odd but tripled
