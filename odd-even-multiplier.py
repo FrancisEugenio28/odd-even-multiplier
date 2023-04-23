4b67f300f1de7fb5c8d0845623a623de2a4f6da7
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-24 02:36:50 +0800

    for those added lines, we are trying to separate the even and odd. plus we created empty list for our even and odd.

--- odd-even-multiplier.py
+++ odd-even-multiplier.py
@@ -5,6 +5,14 @@
 # open and read the source file
 with open("integers.txt", "r") as source_file:
     numbers = [int(num) for num in source_file.read().split()]
+#create a empty list for both odd and even
+odd = []
+even = []
 # separate the even and odd in the source file
+for num in numbers:
+    if num % 2 == 0:
+        even.append(num)
+    else:
+        odd.append(num)
 # create the new txt file for evem but doubled
 # create the new txt file for odd but tripled
